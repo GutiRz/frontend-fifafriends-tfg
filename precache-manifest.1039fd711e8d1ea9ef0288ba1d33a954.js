f937fa7a5a7f37d0098e4089cbe6650748bd173c
Germán Gutiérrez <gutconde@gmail.com>
2020-09-22 21:53:57 +0200

    Updates

---
+++ precache-manifest.1039fd711e8d1ea9ef0288ba1d33a954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1eace611f898f4a58dcaad43294389",
+    "url": "/frontend-fifafriends-tfg/index.html"
+  },
+  {
+    "revision": "ea4bebc78ce1bc415085",
+    "url": "/frontend-fifafriends-tfg/static/css/main.a889d10a.chunk.css"
+  },
+  {
+    "revision": "680bceba14d1780c8587",
+    "url": "/frontend-fifafriends-tfg/static/js/2.c3ef977d.chunk.js"
+  },
+  {
+    "revision": "59be0cfe97b137aaaf63edc61870b5a0",
+    "url": "/frontend-fifafriends-tfg/static/js/2.c3ef977d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4bebc78ce1bc415085",
+    "url": "/frontend-fifafriends-tfg/static/js/main.d50c4f14.chunk.js"
+  },
+  {
+    "revision": "ee2d5c21a9e40abd0b77",
+    "url": "/frontend-fifafriends-tfg/static/js/runtime-main.137d9a5b.js"
+  }
+]);
